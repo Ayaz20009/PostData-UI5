74301e476efd2a75d7844ce9df9430bbe16b47f8
ayaz20009 <ayazshah_20009@yahoo.com>
2017-10-24 19:26:53 +0000

    Set up Add controller page

--- webapp/controller/Add.controller.js
+++ webapp/controller/Add.controller.js
@@ -1,12 +1,30 @@
 sap.ui.define([
-		"testing/controller/BaseController"
-	], function (BaseController) {
+		"testing/controller/BaseController",
+		"sap/ui/core/routing/History"
+	], function (BaseController,History) {
 		"use strict";
 
 		return BaseController.extend("testing.controller.Add", {
-	
-
-		});
-
-	}
-);
+		
+		onInit: function() {
+			// Register to the add route matched
+			this.getRouter().getRoute("add").attachPatternMatched(this._onRouteMatched, this);
+		},
+		_onRouteMatched: function() {
+		
+			
+		},
+		onNavBack : function() {
+			var oHistory = History.getInstance(),
+			sPreviousHash = oHistory.getPreviousHash();
+			if (sPreviousHash !== undefined) {
+				// The history contains a previous entry
+				history.go(-1);
+			} else {
+				// Otherwise we go backwards with a forward history
+				var bReplace = true;
+				this.getRouter().navTo("worklist", {}, bReplace);
+			}
+		}
+	});
+});
